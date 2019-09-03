b3cf64e50ec87209079e840d4c705723e8ae96e8
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-09-03 13:16:47 -0700

    Added simple cov and lib files to test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
-# Add simplecov
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test/' # Tests should not be counted toward coverage.
+end
+
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
@@ -6,3 +10,8 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+require_relative '../lib/date'
+require_relative '../lib/hotel_block'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
+require_relative '../lib/system'
